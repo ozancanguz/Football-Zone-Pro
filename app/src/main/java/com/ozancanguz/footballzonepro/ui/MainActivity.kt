fb1afe139fea3327e22c8976862e4877f3e3b5ba
Ozan <ozancangu24@gmail.com>
2023-04-15 23:45:31 +0300

    -bottom navigation setup created

--- app/src/main/java/com/ozancanguz/footballzonepro/ui/MainActivity.kt
+++ app/src/main/java/com/ozancanguz/footballzonepro/ui/MainActivity.kt
@@ -2,11 +2,40 @@ package com.ozancanguz.footballzonepro.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ozancanguz.footballzonepro.R
+import com.ozancanguz.footballzonepro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.resultsFragment,
+                R.id.rankingFragment,
+                R.id.goalKingFragment
+            )
+        )
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
