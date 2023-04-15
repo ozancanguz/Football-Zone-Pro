c01022aa40233328f66b70f57d6e0d5ec8c689cb
Ozan <ozancangu24@gmail.com>
2023-04-15 23:47:48 +0300

    -Dagger hilt added

---
+++ app/src/main/java/com/ozancanguz/footballzonepro/ui/HiltApplication.kt
@@ -0,0 +1,10 @@
+package com.ozancanguz.footballzonepro.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication:Application() {
+
+
+}
