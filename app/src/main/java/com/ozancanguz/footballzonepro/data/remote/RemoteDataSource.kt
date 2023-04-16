71c278cb80910ca344947c4a418af868db6d8281
Ozan <ozancangu24@gmail.com>
2023-04-16 15:59:11 +0300

    -Remote data source getRanking added

--- app/src/main/java/com/ozancanguz/footballzonepro/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/ozancanguz/footballzonepro/data/remote/RemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.ozancanguz.footballzonepro.data.remote
 
 import android.util.Log
+import com.ozancanguz.footballzonepro.data.models.rankings.Rankings
 import com.ozancanguz.footballzonepro.data.results.LatestResults
 import com.ozancanguz.footballzonepro.data.results.api.Footballzoneapi
 
@@ -18,6 +19,10 @@ class RemoteDataSource@Inject constructor(private val footballzoneapi: Footballz
             Log.e("RemoteDataSource", "API request error: ${response.code()} - ${response.errorBody()?.string()}")
             return Response.error(response.code(), response.errorBody()!!)
         }
+    }
+
+    suspend fun getRankings():Response<Rankings>{
+        return footballzoneapi.getRankings()
     }
 
 
