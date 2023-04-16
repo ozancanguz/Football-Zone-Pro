d87758bb7e2d53420ba9dca3a821fb7a8da92b08
Ozan <ozancangu24@gmail.com>
2023-04-16 18:41:58 +0300

    -data observed and ui updated

--- app/src/main/java/com/ozancanguz/footballzonepro/ui/fragments/goalking/GoalKingFragment.kt
+++ app/src/main/java/com/ozancanguz/footballzonepro/ui/fragments/goalking/GoalKingFragment.kt
@@ -5,19 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ozancanguz.footballzonepro.R
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ozancanguz.footballzonepro.data.adapters.GoalKingsAdapter
+import com.ozancanguz.footballzonepro.databinding.FragmentGoalKingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GoalKingFragment : Fragment() {
+    private var _binding: FragmentGoalKingBinding? = null
+
+    private val binding get() = _binding!!
+
+    // init view model
+    private val goalKingViewModel:GoalKingsViewModel by viewModels()
+
+    // init adapter
+    private val goalKingsAdapter=GoalKingsAdapter()
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_goal_king, container, false)
+        _binding = FragmentGoalKingBinding.inflate(inflater, container, false)
+
+
+        // setting up rv
+        setupRv()
+
+
+        // observe live data and update ui
+        observeGoalKingLiveData()
+
+
+
+        return binding.root
+    }
+
+    private fun setupRv() {
+        binding.goalKingRv.layoutManager=LinearLayoutManager(requireContext())
+        binding.goalKingRv.adapter=goalKingsAdapter
+    }
+
+    private fun observeGoalKingLiveData(){
+        goalKingViewModel.getGoalKings()
+        goalKingViewModel.goalKingsList.observe(viewLifecycleOwner, Observer {
+            goalKingsAdapter.setGoalKingsData(it)
+        })
     }
 
 
