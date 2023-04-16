365331ed8bff9e1a6b3e2d0883e152b73cca68c7
Ozan <ozancangu24@gmail.com>
2023-04-16 18:17:33 +0300

    -footballzoneapi.kt getGoal service created

--- app/src/main/java/com/ozancanguz/footballzonepro/data/results/api/Footballzoneapi.kt
+++ app/src/main/java/com/ozancanguz/footballzonepro/data/results/api/Footballzoneapi.kt
@@ -1,5 +1,6 @@
 package com.ozancanguz.footballzonepro.data.results.api
 
+import com.ozancanguz.footballzonepro.data.models.goalkings.GoalKings
 import com.ozancanguz.footballzonepro.data.models.rankings.Rankings
 import com.ozancanguz.footballzonepro.data.results.LatestResults
 import retrofit2.Response
@@ -15,5 +16,11 @@ interface Footballzoneapi {
     @Headers("authorization: apikey 2xAW88zA1iemGAxzWMNumI:47IyS3uICCasjqzWaFtxnn", "content-type: application/json")
     @GET("league?data.league=super-lig")
     suspend fun getRankings():Response<Rankings>
+
+    @Headers("authorization: apikey 2xAW88zA1iemGAxzWMNumI:47IyS3uICCasjqzWaFtxnn", "content-type: application/json")
+    @GET("goalKings?data.league=super-lig")
+    suspend fun getGoalKings():Response<GoalKings>
+
+
 
 }
