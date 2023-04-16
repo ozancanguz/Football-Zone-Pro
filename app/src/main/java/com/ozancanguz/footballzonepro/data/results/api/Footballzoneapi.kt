56ddb168e38768217555a74d55571ae32db3ba1e
Ozan <ozancangu24@gmail.com>
2023-04-16 15:58:14 +0300

    -Footballzoneapi.kt ranking request function created

--- app/src/main/java/com/ozancanguz/footballzonepro/data/results/api/Footballzoneapi.kt
+++ app/src/main/java/com/ozancanguz/footballzonepro/data/results/api/Footballzoneapi.kt
@@ -1,5 +1,6 @@
 package com.ozancanguz.footballzonepro.data.results.api
 
+import com.ozancanguz.footballzonepro.data.models.rankings.Rankings
 import com.ozancanguz.footballzonepro.data.results.LatestResults
 import retrofit2.Response
 import retrofit2.http.GET
@@ -10,4 +11,9 @@ interface Footballzoneapi {
     @Headers("authorization: apikey 2xAW88zA1iemGAxzWMNumI:47IyS3uICCasjqzWaFtxnn", "content-type: application/json")
     @GET("results?data.league=super-lig")
     suspend fun getResults(): Response<LatestResults>
+
+    @Headers("authorization: apikey 2xAW88zA1iemGAxzWMNumI:47IyS3uICCasjqzWaFtxnn", "content-type: application/json")
+    @GET("league?data.league=super-lig")
+    suspend fun getRankings():Response<Rankings>
+
 }
